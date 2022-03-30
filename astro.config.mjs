057241deba102de90c502b24bebfe72ce70de1aa
Edvin Linden <edvin.linden@fortnox.se>
2022-03-30 15:38:58 +0200

    Add react and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	buildOptions: {
-		site: "http://edvinlinden.se/killedby.tech",
+		site: "https://killedby.tech",
 	},
+	devOptions: {
+		tailwindConfig: "./tailwind.config.js",
+	},
+	integrations: [react(), tailwind()],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			sans: ["Helvetica", "Arial", "sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
