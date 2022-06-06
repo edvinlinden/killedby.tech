7631ac38e5c58ccede5869dc3290043d46801ed9
Edvin Linden <edvin.linden@gmail.com>
2022-06-06 15:16:46 +0200

    Fix sitemap according to Astro 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://killedby.tech",
-	integrations: [react()],
+	integrations: [react(), sitemap()],
 });
