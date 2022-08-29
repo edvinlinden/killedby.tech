ed051db16703b09cc92396b1a691e55a054e1bbd
Edvin Linden <edvin.linden@gmail.com>
2022-08-29 22:12:00 +0200

    Update 'To The Top' button to become visible when scrolling

--- src/components/OverlayButtons/OverlayButtons.tsx
+++ src/components/OverlayButtons/OverlayButtons.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import useScrollPosition from "@hooks/useScrollPosition";
 import styles from "./OverlayButtons.module.scss";
 
 const OverlayButtons = () => {
+	const scrollPosition = useScrollPosition();
+	const shouldShowToTopButton = scrollPosition > 400;
+
 	return (
 		<>
-			<a className={styles.toTheTop} href="#top" title="To the top">
-				To the top
-			</a>
+			{shouldShowToTopButton && (
+				<a className={styles.toTheTop} href="#top" title="To the top">
+					To the top
+				</a>
+			)}
 			<a
 				className={styles.aboutButton}
 				href="#about"
